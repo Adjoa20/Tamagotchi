9dc4fae29685723b7c31bdeba774207bf6641589
agathaa <aamoako1120@gmail.com>
2023-09-28 21:53:41 -0400

    created Tamagotchi class

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert("JS Linked")
+class Tamagotchi{
+    constructor(hunger, sleepines, boredem, age) {
+        this.hunger = hunger;
+        this.sleepines = sleepines;
+        this.boredem = boredem;
+        this.age = age;
+    }
+}
+const Pearl = new Tamagotchi(4, 6, 2, 4)
+
