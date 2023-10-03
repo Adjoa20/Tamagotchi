f52dd0929f0dd1576202efb54480655b911476a6
agathaa <aamoako1120@gmail.com>
2023-10-03 13:36:42 -0400

    add increase value

--- script.js
+++ script.js
@@ -10,39 +10,62 @@ class Tamagotchi{
         this.sleepines = 6;
         this.boredom = 2;
         this.age = 4;
+        this.dead = false;
     }
     feedPet(){
         if (this.hunger > 4){
-            this.hunger++
+            this.hunger--
         }
     }
     
     sleepPet(){
         if (this.sleep > 6){
-          this.sleep++  
+          this.sleep-- 
         }
     }
     
     boredPet(){
         if (this.boredem < 2){
-            this.boredem++
+            this.boredem--
         }
     }
     
     agedPet(){
         if(this.age > 4){
-            this.aged++
+            this.aged--
         }
     }
 }
+
+function increaseUp(){
+    this.hunger++;
+    this.sleepines++;
+    this.boredom++;
+    this.age++;
+    if (this.hunger >=10 || this.sleepines >=10 || this.boredom >=10){
+        alert("Your Pet Has Died")
+    }
+}
+
 
 
 // ADDED IN EVENT LISTENERS
 const hungerButton = document.querySelector(".hungerbutton")
 hungerButton.addEventListener("click", () => {
-    Tamagotchi.hunger
+    this.hunger
 })
 
 const sleepButton = document.querySelector(".sleepbutton")
+sleepButton.addEventListener("click", () => {
+    this.sleepines
+})
+
 const boredButton = document.querySelector(".boredbutton")
+boredButton.addEventListener("click", () => {
+    this.boredom
+})
+
 const ageButton = document.querySelector(".agebutton")
+ageButton.addEventListener("click", () => {
+    this.age
+})
