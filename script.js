375d7de8ab584e6c993871c9e93163b79660fd5a
agathaa <aamoako1120@gmail.com>
2023-10-04 12:36:48 -0400

    added in values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // alert("JS is linked!")
 let titleElement = document.getElementById('title2')
-// let petName = prompt('what is your pet name?')
+let petName = prompt('what is your pet name?')
 titleElement.innerHTML = `Welcome To ${petName}'s Room!`
 
 class Tamagotchi{
@@ -13,7 +13,33 @@ class Tamagotchi{
         this.isAlive = true;
     }
 //  ADDING METHODS TO INCREASE HUNGER, SLEEP, ETC 
+increaseValues(){
+    const vauleIncreaseInterval = setInterval(() => {
+        if (this.isAlive){
+            this.hunger++;
+            this.sleepiness++;
+            this.boredom++;
 
+            // CHECK TO SEE IF PET IS DEAD 
+            this.yourPetDied();
+            // THIS IS TO DISPLAY THE METRICS 
+            console.log(`${this.name}'s hunger level is at: ${this.hunger}`);
+            console.log(`${this.name}'s sleepiness level is at: ${this.sleepiness}`);
+            console.log(`${this.name}'s wants to be played with: ${this.boredom}`);
+        } else {
+            // IF THE PET IS DEAD, CLEAR THE INTERVAL 
+            clearInterval(vauleIncreaseInterval);
+        }
+    }, 9000)
+  }
+}
+
+// NEED TO CHECK'S PET'S DEATH 
+yourPetDied() {
+    if (this.hunger >=10 || this.sleepiness >=10 ||  this.boredom >=10){
+        this.isAlive = false;
+        console.log(`${this.name} has died! WHOOMP WHOOMP`)
+    }
 }
 
 // INSTANTIATED THE TAMAGOTCHI
